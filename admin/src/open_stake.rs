40713b775818e10c24d8a57d9aea2acb6da60341
alex <aabinaei@gmail.com>
2024-10-29 02:58:32 -0700

    admin idempotent open-stake

--- admin/src/open_stake.rs
+++ admin/src/open_stake.rs
@@ -1,7 +1,8 @@
+use ore_boost_api::state::Stake;
 use solana_client::nonblocking::rpc_client::RpcClient;
-use solana_sdk::{pubkey::Pubkey, signature::Keypair, signer::Signer, transaction::Transaction};
+use solana_sdk::{pubkey::Pubkey, signature::Keypair, signer::Signer};
 
-use crate::error::Error;
+use crate::{error::Error, get_or_create};
 
 pub async fn open_stake(
     rpc_client: &RpcClient,
@@ -10,11 +11,9 @@ pub async fn open_stake(
 ) -> Result<(), Error> {
     let mint = mint.ok_or(Error::MissingBoostMint)?;
     let pubkey = keypair.pubkey();
+    let (pool_pda, _) = ore_pool_api::state::pool_pda(pubkey);
+    let (stake_pda, _) = ore_pool_api::state::pool_stake_pda(pool_pda, mint);
     let ix = ore_pool_api::sdk::open_stake(pubkey, mint);
-    let mut tx = Transaction::new_with_payer(&[ix], Some(&pubkey));
-    let hash = rpc_client.get_latest_blockhash().await?;
-    tx.sign(&[keypair], hash);
-    let sig = rpc_client.send_transaction(&tx).await?;
-    println!("{:?}", sig);
+    get_or_create::pda::<Stake>(rpc_client, keypair, &stake_pda, ix).await?;
     Ok(())
 }
