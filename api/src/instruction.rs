936e76572c75d6722bba108b9d6c2e18fed67388
Hardhat Chad <hardhatchad@gmail.com>
2024-10-30 17:57:33 +0000

    migration phase 3

--- api/src/instruction.rs
+++ api/src/instruction.rs
@@ -16,10 +16,6 @@ pub enum PoolInstruction {
     Launch = 102,
     OpenStake = 103,
     Submit = 104,
-
-    // Migration
-    MigratePool = 200,
-    MigrateMemberBalance = 201,
 }
 
 #[repr(C)]
@@ -83,14 +79,6 @@ pub struct Unstake {
     pub amount: [u8; 8],
 }
 
-#[repr(C)]
-#[derive(Clone, Copy, Debug, Pod, Zeroable)]
-pub struct MigratePool {}
-
-#[repr(C)]
-#[derive(Clone, Copy, Debug, Pod, Zeroable)]
-pub struct MigrateMemberBalance {}
-
 instruction!(PoolInstruction, Attribute);
 instruction!(PoolInstruction, Claim);
 instruction!(PoolInstruction, Commit);
@@ -101,5 +89,3 @@ instruction!(PoolInstruction, Join);
 instruction!(PoolInstruction, Stake);
 instruction!(PoolInstruction, Submit);
 instruction!(PoolInstruction, Unstake);
-instruction!(PoolInstruction, MigratePool);
-instruction!(PoolInstruction, MigrateMemberBalance);
