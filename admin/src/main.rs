7558ad9ceec992fcc3fe7b5f47076afcd315bc82
Hardhat Chad <hardhatchad@gmail.com>
2024-12-23 11:37:10 -0600

    remove open stake from admin tool

--- admin/src/main.rs
+++ admin/src/main.rs
@@ -1,15 +1,12 @@
-use std::str::FromStr;
-
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{
-    commitment_config::CommitmentConfig, pubkey::Pubkey, signature::Keypair, signer::EncodableKey,
+    commitment_config::CommitmentConfig, signature::Keypair, signer::EncodableKey,
 };
 
 mod error;
 mod get_or_create;
 mod init;
 mod member_account;
-mod open_stake;
 mod pool_account;
 mod proof_account;
 
@@ -19,12 +16,10 @@ async fn main() -> Result<(), error::Error> {
     let command = command()?;
     let keypair = keypair()?;
     let rpc_client = rpc_client()?;
-    let boost_mint = boost_mint();
     let pool_url = pool_url();
     // run
     match command.as_str() {
         "init" => init::init(&rpc_client, &keypair, pool_url).await,
-        "open-stake" => open_stake::open_stake(&rpc_client, &keypair, boost_mint).await,
         "pool-account" => pool_account::pool_account(&rpc_client, &keypair).await,
         "proof-account" => proof_account::proof_account(&rpc_client, &keypair).await,
         "member-account" => member_account::member_account(&rpc_client, &keypair).await,
@@ -34,17 +29,6 @@ async fn main() -> Result<(), error::Error> {
 
 fn command() -> Result<String, error::Error> {
     std::env::var("COMMAND").map_err(From::from)
-}
-
-fn boost_mint() -> Option<Pubkey> {
-    std::env::var("MINT").ok().and_then(|mint| {
-        Pubkey::from_str(mint.as_str())
-            .map_err(|err| {
-                println!("{:?}", err);
-                err
-            })
-            .ok()
-    })
 }
 
 fn rpc_client() -> Result<RpcClient, error::Error> {
