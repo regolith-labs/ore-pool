0a95705a10f8f2df1603ff21348c6f31410465be
Hardhat Chad <hardhatchad@gmail.com>
2025-01-09 10:13:10 -0800

    phase 2 bugfix

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -50,71 +50,71 @@ pub async fn main() {
     };
 
     // TODO Phase 1: Initialize migration
-    for pool in pools {
-        println!("Pool: {:?}", pool.0);
-        let ix = ore_pool_api::sdk::migrate_pool(signer.pubkey(), pool.0);
-        let cu_limit_ix = ComputeBudgetInstruction::set_compute_unit_limit(200_000);
-        let cu_price_ix = ComputeBudgetInstruction::set_compute_unit_price(10_000);
-        let final_ixs = &[cu_limit_ix, cu_price_ix, ix];
-        let hash = rpc.get_latest_blockhash().await.unwrap();
-        let mut tx = Transaction::new_with_payer(final_ixs.as_slice(), Some(&signer.pubkey()));
-        tx.sign(&[&signer], hash);
-        println!("{:?}", tx);
-        // rpc.send_transaction(&tx).await.unwrap();
-    }
+    // for pool in pools {
+    //     println!("Pool: {:?}", pool.0);
+    //     let ix = ore_pool_api::sdk::migrate_pool(signer.pubkey(), pool.0);
+    //     let cu_limit_ix = ComputeBudgetInstruction::set_compute_unit_limit(200_000);
+    //     let cu_price_ix = ComputeBudgetInstruction::set_compute_unit_price(10_000);
+    //     let final_ixs = &[cu_limit_ix, cu_price_ix, ix];
+    //     let hash = rpc.get_latest_blockhash().await.unwrap();
+    //     let mut tx = Transaction::new_with_payer(final_ixs.as_slice(), Some(&signer.pubkey()));
+    //     tx.sign(&[&signer], hash);
+    //     println!("{:?}", tx);
+    //     // rpc.send_transaction(&tx).await.unwrap();
+    // }
 
     // TODO: Phase 2: Migrate member balances
-    // for pool in pools {
-    //     // Fetch pool
-    //     let pool_account = Pool::try_from_bytes(&pool.1.data).unwrap();
-    //     println!("Pool: {}", pool.0);
+    for pool in pools {
+        // Fetch pool
+        let pool_account = Pool::try_from_bytes(&pool.1.data).unwrap();
+        println!("Pool: {}", pool.0);
 
-    //     // Fetch members of the given pool
-    //     let member_filter = RpcFilterType::Memcmp(Memcmp::new_raw_bytes(
-    //         0,
-    //         Member::discriminator().to_le_bytes().to_vec(),
-    //     ));
-    //     let pool_member_filter =
-    //         RpcFilterType::Memcmp(Memcmp::new_raw_bytes(16, pool.0.to_bytes().to_vec()));
-    //     let members = match rpc
-    //         .get_program_accounts_with_config(
-    //             &ore_pool_api::ID,
-    //             RpcProgramAccountsConfig {
-    //                 filters: Some(vec![member_filter, pool_member_filter]),
-    //                 account_config: RpcAccountInfoConfig {
-    //                     encoding: Some(UiAccountEncoding::Base64),
-    //                     data_slice: None,
-    //                     commitment: None,
-    //                     min_context_slot: None,
-    //                 },
-    //                 with_context: None,
-    //             },
-    //         )
-    //         .await
-    //     {
-    //         Ok(members) => {
-    //             println!("Expected members: {}", pool_account.total_members);
-    //             println!("Actual members: {}\n", members.len());
-    //             members
-    //         }
-    //         Err(e) => {
-    //             println!("Error fetching members: {:?}", e);
-    //             return;
-    //         }
-    //     };        
+        // Fetch members of the given pool
+        let member_filter = RpcFilterType::Memcmp(Memcmp::new_raw_bytes(
+            0,
+            Member::discriminator().to_le_bytes().to_vec(),
+        ));
+        let pool_member_filter =
+            RpcFilterType::Memcmp(Memcmp::new_raw_bytes(16, pool.0.to_bytes().to_vec()));
+        let members = match rpc
+            .get_program_accounts_with_config(
+                &ore_pool_api::ID,
+                RpcProgramAccountsConfig {
+                    filters: Some(vec![member_filter, pool_member_filter]),
+                    account_config: RpcAccountInfoConfig {
+                        encoding: Some(UiAccountEncoding::Base64),
+                        data_slice: None,
+                        commitment: None,
+                        min_context_slot: None,
+                    },
+                    with_context: None,
+                },
+            )
+            .await
+        {
+            Ok(members) => {
+                println!("Expected members: {}", pool_account.total_members);
+                println!("Actual members: {}\n", members.len());
+                members
+            }
+            Err(e) => {
+                println!("Error fetching members: {:?}", e);
+                return;
+            }
+        };        
 
-    //     // Migrate each member balance
-    //     for member in members {
-    //         let ix = ore_pool_api::sdk::migrate_member_balance(signer.pubkey(), pool.0, member.0);
-    //         let cu_limit_ix = ComputeBudgetInstruction::set_compute_unit_limit(200_000);
-    //         let cu_price_ix = ComputeBudgetInstruction::set_compute_unit_price(10_000);
-    //         let final_ixs = &[cu_limit_ix, cu_price_ix, ix];
-    //         let hash = rpc.get_latest_blockhash().await.unwrap();
-    //         let mut tx = Transaction::new_with_payer(final_ixs.as_slice(), Some(&signer.pubkey()));
-    //         tx.sign(&[&signer], hash);
-    //         println!("Migrate member balance: {:?}", tx);
-    //         // rpc.send_transaction(&tx).await.unwrap();
-    //     }
-    // }
+        // Migrate each member balance
+        for member in members {
+            let ix = ore_pool_api::sdk::migrate_member_balance(signer.pubkey(), pool.0, member.0);
+            let cu_limit_ix = ComputeBudgetInstruction::set_compute_unit_limit(200_000);
+            let cu_price_ix = ComputeBudgetInstruction::set_compute_unit_price(10_000);
+            let final_ixs = &[cu_limit_ix, cu_price_ix, ix];
+            let hash = rpc.get_latest_blockhash().await.unwrap();
+            let mut tx = Transaction::new_with_payer(final_ixs.as_slice(), Some(&signer.pubkey()));
+            tx.sign(&[&signer], hash);
+            println!("Migrate member balance: {:?}", tx);
+            // rpc.send_transaction(&tx).await.unwrap();
+        }
+    }
 }
 
